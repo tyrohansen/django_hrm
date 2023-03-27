c77ebae7e623f6ffa70c9e800c32921b33b282f6
Opad Yusuf <yusuf@Opads-MacBook-Pro.local>
2023-03-27 22:38:15 +0300

    feature: added test for departments

---
+++ org/tests/factories.py
@@ -0,0 +1,22 @@
+from org.models import Department
+import factory
+from django.contrib.auth import get_user_model
+from faker import Factory
+
+User = get_user_model()
+faker = Factory.create()
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+    username = faker.email()
+    email = faker.email()
+    password = faker.password()
+
+
+class DepartmentFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Department 
+
+    name = factory.Sequence(lambda n: 'Department %d' % n)
+    shortname=factory.Sequence(lambda n: 'dpt%d' % n)
+    author = factory.SubFactory(UserFactory)
---
+++ org/tests/test_endpoint.py
@@ -0,0 +1,82 @@
+import json
+import pytest
+from django.contrib.auth import get_user_model
+from .factories import DepartmentFactory, UserFactory
+
+pytestmark = pytest.mark.django_db
+
+class TestDepartment:
+    endpoint = "/api/org/departments/"
+    def setUp(self):
+        self.user = UserFactory()
+
+    def test_list_departments(self, api_client):
+        user = UserFactory()
+        departments = DepartmentFactory.create_batch(
+                             3, 
+                             author=user
+                            )
+        client = api_client()
+        client.force_authenticate(user=user)
+        response = client.get(
+            self.endpoint
+        )
+        assert response.status_code == 200
+        assert len(response.json()) == 3
+
+    def test_retrieve_department(self, api_client):
+        user = UserFactory()
+        department = DepartmentFactory(author=user)
+        url = f'{self.endpoint}{department.id}/'
+        client = api_client()
+        client.force_authenticate(user=user)
+        response = client.get(url)
+        assert response.status_code == 200
+        assert response.json()['name'] == department.name
+
+    def test_create_department(self, api_client):
+        user = UserFactory()
+        department_json = {
+            'name': "Department one",
+            'shortname': "DPT1"
+        }
+        client = api_client()
+        client.force_authenticate(user=user)
+        response = client.post(self.endpoint, department_json, format='json')
+        assert response.status_code == 201
+
+    def test_update_department(self, api_client):
+        user = UserFactory()
+        department = DepartmentFactory(author=user)
+        department_updated = {
+            'name': 'Update department name',
+            'shortname': 'UDPT1'
+        }
+        url = f'{self.endpoint}{department.id}/'
+        client = api_client()
+        client.force_authenticate(user=user)
+        response = client.put(url, department_updated, format='json')
+        print(response.content)
+        assert response.status_code == 200
+        assert response.json()['name'] == department_updated['name']
+
+    def test_delete_department(self, api_client):
+        user = UserFactory()
+        department = DepartmentFactory(author=user)
+        url = f'{self.endpoint}{department.id}/'
+        client = api_client()
+        client.force_authenticate(user=user)
+        response = client.delete(url)
+        assert response.status_code == 204
+
+    def test_authenticated_list_department(self, api_client):
+        user = UserFactory()
+        departments = DepartmentFactory.create_batch(
+                             3, 
+                             author=user
+                            )
+        client = api_client()
+        response = client.get(
+            self.endpoint
+        )
+        assert response.status_code == 403
